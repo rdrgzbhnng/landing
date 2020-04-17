7c2413c9b1e8cbaabf8c6b19ea13378275fd3a53
Carlos Alberto <oiueei@disroot.org>
2020-04-17 20:11:07 +0200

    Add resume and change views to routes

--- rdbh/app.py
+++ rdbh/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from rdbh.extensions import configuration, appearance
-from rdbh.blueprints import views
+from rdbh.blueprints import routes
 
 
 app = Flask(__name__)
 
 configuration.init_app(app)
 appearance.init_app(app)
-views.init_app(app)
+routes.init_app(app)
--- rdbh/blueprints/routes.py
+++ rdbh/blueprints/routes.py
@@ -4,7 +4,11 @@ from flask import render_template
 def init_app(app):
     @app.route('/')
     def index():
-        return render_template("index.html")
+        return render_template("home.html")
+
+    @app.route('/resume')
+    def resume():
+        return render_template("resume.html")
 
     if __name__ == '__main__':
         app.run()
