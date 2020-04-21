10bc04e544bb2c643396fc5c2cdbdf9b473cb5ab
Carlos Alberto <oiueei@disroot.org>
2020-04-21 12:03:16 +0200

    Add book and cv redirects

--- rdbh/blueprints/webui/views.py
+++ rdbh/blueprints/webui/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 
 def home():
@@ -7,9 +7,18 @@ def home():
 def resume():
     return render_template("resume.html")
 
+def book():
+    return redirect("https://gitlab.com/rdrgzbhnng/docs/-/raw/master/rdrgzbhnng-book.pdf?inline=false")
+
+def cv():
+    return redirect("https://gitlab.com/rdrgzbhnng/docs/-/raw/master/rdrgzbhnng-cv.pdf?inline=false")
+
+
 bp = Blueprint("webui", __name__, template_folder="templates")
 bp.add_url_rule("/", view_func=home)
 bp.add_url_rule("/resume", view_func=resume)
+bp.add_url_rule("/book", view_func=book)
+bp.add_url_rule("/cv", view_func=cv)
 
 
 def init_app(app):
