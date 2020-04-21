2734b6b76acca586a52e912dd010be1dc3b297ee
Carlos Alberto <oiueei@disroot.org>
2020-04-21 15:46:43 +0200

    Adding templates and static files

--- rdbh/blueprints/webui/views.py
+++ rdbh/blueprints/webui/views.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, redirect
 
-
 def home():
     return render_template("home.html")
 
@@ -14,11 +13,12 @@ def cv():
     return redirect("https://gitlab.com/rdrgzbhnng/docs/-/raw/master/rdrgzbhnng-cv.pdf?inline=false")
 
 
-bp = Blueprint("webui", __name__, template_folder="templates")
+bp = Blueprint("webui", __name__, template_folder="templates", static_folder="static")
 bp.add_url_rule("/", view_func=home)
 bp.add_url_rule("/resume", view_func=resume)
 bp.add_url_rule("/book", view_func=book)
 bp.add_url_rule("/cv", view_func=cv)
+
 
 def init_app(app):
     app.register_blueprint(bp)
